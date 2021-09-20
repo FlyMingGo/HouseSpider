3a8b5939f88a417738b506656e65b0538326c998
Frank Gui <mingming_gui@foxmail.com>
2021-09-20 13:34:49 -0700

    some changes

--- HouseSpider/HouseInfoCollection.cs
+++ HouseSpider/HouseInfoCollection.cs
@@ -17,6 +17,8 @@ namespace HouseSpider
         {
             HouseInfo houseInfo = new HouseInfo();
             PriceDetail price_detail = new PriceDetail();
+            Info info = new Info();
+            HomeFacts homeFacts = new HomeFacts();
             try
             {
                 IWebDriver driver = new ChromeDriver("C:\\Program Files (x86)\\Google\\Chrome\\Application");
@@ -33,25 +35,126 @@ namespace HouseSpider
                     .ToList()[0]
                     .Descendants("section")
                     .ToList()[0].InnerText.Trim();
-
-                price_detail.list_price = root.Descendants()
+                var price_detail_items = root.Descendants()
                     .Where(n => n.GetAttributeValue("class", "")
                     .Contains("sectionblock-display"))
                     .ToList()[0]
-                    .Descendants("tbody").ToList()[0].Descendants("td")
-                    .ToList()[0].InnerText.Trim();
-                price_detail.gross_taxes = root.Descendants()
+                    .Descendants("tbody").ToList()[0].Descendants("tr")
+                    .ToList();
+                foreach (var item in price_detail_items)
+                {
+                    if(item.Descendants("th").ToList()[0].InnerText.Contains("List Price"))
+                    {
+                        price_detail.list_price = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Gross Taxes"))
+                    {
+                        price_detail.gross_taxes = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Strata Maintenance Fees"))
+                    {
+                        price_detail.strata_maintenance_fees = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                }
+                var homeFacts_items = root.Descendants()
                     .Where(n => n.GetAttributeValue("class", "")
                     .Contains("sectionblock-display"))
                     .ToList()[0]
-                    .Descendants("tbody").ToList()[0].Descendants("td")
-                    .ToList()[1].InnerText.Trim();
-                price_detail.strata_maintenance_fees = root.Descendants()
+                    .Descendants("tbody").ToList()[1].Descendants("tr")
+                    .ToList();
+                foreach (var item in homeFacts_items)
+                {
+                    if (item.Descendants("th").ToList()[0].InnerText.Contains("Bedrooms"))
+                    {
+                        homeFacts.bedrooms = Convert.ToInt32(item.Descendants("td").ToList()[0].InnerText.Trim());
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Bathrooms"))
+                    {
+                        homeFacts.bathrooms = Convert.ToInt32(item.Descendants("td").ToList()[0].InnerText.Trim());
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Property Type"))
+                    {
+                        homeFacts.property_type = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Lot Size"))
+                    {
+                        homeFacts.lot_size = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Depth"))
+                    {
+                        homeFacts.depth = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Frontage"))
+                    {
+                        homeFacts.property_type = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Year Built"))
+                    {
+                        homeFacts.year_built = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Title"))
+                    {
+                        homeFacts.title = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Style"))
+                    {
+                        homeFacts.style = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Features"))
+                    {
+                        homeFacts.features = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Amenities"))
+                    {
+                        homeFacts.amenities = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                    else if (item.Descendants("th").ToList()[0].InnerText.Contains("Appliances"))
+                    {
+                        homeFacts.appliances = item.Descendants("td").ToList()[0].InnerText.Trim();
+                    }
+                }
+                homeFacts.bedrooms = Convert.ToInt32(root.Descendants()
                     .Where(n => n.GetAttributeValue("class", "")
                     .Contains("sectionblock-display"))
                     .ToList()[0]
-                    .Descendants("tbody").ToList()[0].Descendants("td")
+                    .Descendants("tbody").ToList()[1].Descendants("td")
+                    .ToList()[0].InnerText.Trim());
+                homeFacts.bathrooms = Convert.ToInt32(root.Descendants()
+                    .Where(n => n.GetAttributeValue("class", "")
+                    .Contains("sectionblock-display"))
+                    .ToList()[0]
+                    .Descendants("tbody").ToList()[1].Descendants("td")
+                    .ToList()[1].InnerText.Trim());
+                homeFacts.property_type = root.Descendants()
+                    .Where(n => n.GetAttributeValue("class", "")
+                    .Contains("sectionblock-display"))
+                    .ToList()[0]
+                    .Descendants("tbody").ToList()[1].Descendants("td")
                     .ToList()[2].InnerText.Trim();
+                homeFacts.year_built = root.Descendants()
+                    .Where(n => n.GetAttributeValue("class", "")
+                    .Contains("sectionblock-display"))
+                    .ToList()[0]
+                    .Descendants("tbody").ToList()[1].Descendants("td")
+                    .ToList()[3].InnerText.Trim();
+                homeFacts.title = root.Descendants()
+                    .Where(n => n.GetAttributeValue("class", "")
+                    .Contains("sectionblock-display"))
+                    .ToList()[0]
+                    .Descendants("tbody").ToList()[1].Descendants("td")
+                    .ToList()[4].InnerText.Trim();
+                homeFacts.style = root.Descendants()
+                    .Where(n => n.GetAttributeValue("class", "")
+                    .Contains("sectionblock-display"))
+                    .ToList()[0]
+                    .Descendants("tbody").ToList()[1].Descendants("td")
+                    .ToList()[5].InnerText.Trim();
+                homeFacts.community = root.Descendants()
+                    .Where(n => n.GetAttributeValue("class", "")
+                    .Contains("sectionblock-display"))
+                    .ToList()[0]
+                    .Descendants("tbody").ToList()[1].Descendants("td")
+                    .ToList()[6].InnerText.Trim();
                 return houseInfo;
             }
             catch (Exception ex)
--- HouseSpider/Model/HomeFacts.cs
+++ HouseSpider/Model/HomeFacts.cs
@@ -9,11 +9,14 @@ namespace HouseSpider.Model
         public int bedrooms { get; set; }
         public int bathrooms { get; set; }
         public string property_type { get; set; }
-        public int year_built { get; set; }
+        public string year_built { get; set; }
         public string title { get; set; }
         public string style { get; set; }
         public string amenities { get; set; }
         public string appliances { get; set; }
         public string community { get; set; }
+        public string lot_size { get; set; }
+        public string depth { get; set; }
+        public string features { get; set; }
     }
 }
